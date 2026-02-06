c5d58307f93ccbb93946e7a139bc412a2fadba9d
vflor <vfloresgonz@gmail.com>
2026-02-06 12:33:12 +0100

    feat: constants and theme creation

---
+++ MeFolder/src/constants/themes/colors.ts
@@ -0,0 +1,33 @@
+import { Colors } from './types';
+
+export const lightColors: Colors = {
+  primary: '#007AFF',
+  primaryDark: '#0056CC',
+  secondary: '#5856D6',
+  background: '#F8F9FA',
+  surface: '#FFFFFF',
+  card: '#FFFFFF',
+  text: '#1C1C1E',
+  textSecondary: '#6D6D70',
+  border: '#E1E1E1',
+  success: '#34C759',
+  warning: '#FF9500',
+  error: '#FF3B30',
+  info: '#007AFF',
+};
+
+export const darkColors: Colors = {
+  primary: '#0A84FF',
+  primaryDark: '#0969DA',
+  secondary: '#5E5CE6',
+  background: '#000000',
+  surface: '#1C1C1E',
+  card: '#2C2C2E',
+  text: '#FFFFFF',
+  textSecondary: '#98989D',
+  border: '#38383A',
+  success: '#32D74B',
+  warning: '#FF9F0A',
+  error: '#FF453A',
+  info: '#64D2FF',
+};
---
+++ MeFolder/src/constants/themes/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './colors';
+export * from './typography';
+export * from './spacing';
+export * from './themes';
+
+export { lightTheme as default } from './themes';
